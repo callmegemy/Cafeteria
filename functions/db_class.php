2977471bb54fbf9d089e979b6bbe4a29edb8f061
“callmegemy” <callmegemy@gmail.com>
2024-08-15 16:11:57 +0300

    Home updates

--- functions/db_class.php
+++ functions/db_class.php
@@ -82,5 +82,46 @@ class Database
     public function lastInsertId() {
         return $this->conn->lastInsertId();
     }
+    public function checkIfUserExists($table, $email)
+    {
+        $sql = "SELECT * FROM $table WHERE email = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$email]);
+
+        // Check if any row was returned
+        // Fetch the user ID
+        $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $userData !== false ? $userData : null;
+    }
+
+    public function changePasswordByID($randomNumber, $newPassword)
+    {
+        $user_id = $_COOKIE['user_id'];
+        $codeRow = $this->getRow("forget_password", "user_id",  $user_id);
+        if ($codeRow !== false) {
+            if ($randomNumber == $codeRow['random_number']) {
+                $this->update('users', "password = $newPassword", $user_id);
+                $this->delete('forget_password', $codeRow['id']);
+                header("location:../home.php");
+            } else {
+                echo "you have entered wrong or expired code please try again";
+            }
+        } else {
+            echo "there's no code";
+        }
+    }
+
+    public function getLatestUserProducts($user_id) {
+        $sql = "SELECT p.*, op.order_id, o.date 
+                FROM products p 
+                JOIN orders_products op ON p.id = op.product_id 
+                JOIN orders o ON op.order_id = o.id 
+                WHERE o.user_id = ? 
+                ORDER BY o.date DESC, op.order_id DESC 
+                LIMIT 3";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$user_id]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 ?>
